98dd49ee740feca834edd9f72063a7a7fcf1d88b
Raul Delgado <raul.delgado.diaz97@gmail.com>
2020-01-02 08:37:34 +0100

    Add Simple Interest page and logic

---
+++ Project1/Calculator/SimpleInterest/SImpleInterest.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function calculate(e) {
+    e.preventDefault();
+    let p = document.getElementById("p").value;
+    let y = document.getElementById("y").value;
+    let r = document.getElementById("r").value;
+    let result = document.getElementById("result");
+
+    let interest = p * (r / 100) * y;
+
+    result.innerHTML = "The total amount is " + (interest + parseInt(p));
+    result.innerHTML += "<br> The interest is " + interest;
+
+}
