a82e12eb258b6f0799fa8be9adc3df813444889a
Raul Delgado <raul.delgado.diaz97@gmail.com>
2020-01-07 04:21:51 +0100

    Add change icon on click at rate value page

--- Project1/Calculator/ReturnRate/ReturnRate.js
+++ Project1/Calculator/ReturnRate/ReturnRate.js
@@ -15,10 +15,16 @@ function calculate(e) {
 
 function showInfo() {
     let card = document.getElementById('card');
+    let arrow1 = document.getElementById('arrow1')
+    let arrow2 = document.getElementById('arrow2')
 
     if (card.style.display === 'flex') {
         card.style.display = 'none';
+        arrow1.className = "fas fa-arrow-right";
+        arrow2.className = "fas fa-arrow-left";
     } else {
         card.style.display = 'flex';
+        arrow1.className = "fas fa-arrow-down";
+        arrow2.className = "fas fa-arrow-down";
     }
 }
