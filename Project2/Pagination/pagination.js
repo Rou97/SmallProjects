afdc899ed17a1b07ba780301ab8e8af8ec36a51d
Raul Delgado <raul.delgado.diaz97@gmail.com>
2020-01-19 05:46:50 +0100

    Add logic to arrows in Project2/Pagination

--- Project2/Pagination/pagination.js
+++ Project2/Pagination/pagination.js
@@ -3,11 +3,12 @@
 let setPage = document.getElementById('page1');
 setPage.style.backgroundColor = 'black';
 setPage.style.color = 'white';
+let numberOfPages = 4;
 
 function clicked(id) {
     let actualPage = document.getElementById(id)
 
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i <= numberOfPages; i++) {
         let changePage = document.getElementById(`page${i}`)
         changePage.style.backgroundColor = 'white';
         changePage.style.color = 'black';
@@ -19,5 +20,44 @@ function clicked(id) {
     } else {
         actualPage.style.backgroundColor = 'white';
         actualPage.style.color = 'black';
+    }
+}
+
+function clickedArrow(id) {
+
+    for (let i = 1; i <= numberOfPages; i++) {
+        let actualPage = document.getElementById(`page${i}`);
+
+        if (actualPage.style.backgroundColor === 'black' && id === 'right') {
+
+            if (i === numberOfPages) {
+                break;
+            }
+
+            let changePage = document.getElementById(`page${i + 1}`);
+
+            actualPage.style.backgroundColor = 'white';
+            actualPage.style.color = 'black';
+            changePage.style.backgroundColor = 'black';
+            changePage.style.color = 'white';
+
+            break;
+
+        } else if (actualPage.style.backgroundColor === 'black' && id === 'left') {
+
+            if (i === 1) {
+                break;
+            }
+
+            let changePage = document.getElementById(`page${i - 1}`);
+
+            actualPage.style.backgroundColor = 'white';
+            actualPage.style.color = 'black';
+            changePage.style.backgroundColor = 'black';
+            changePage.style.color = 'white';
+
+            break;
+        }
+
     }
 }
