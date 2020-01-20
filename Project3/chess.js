2a928ebb12b5aea118f0b0caac6253321194e185
Raul Delgado <raul.delgado.diaz97@gmail.com>
2020-01-20 12:30:55 +0100

    Add chess piece to Project3

--- Project3/chess.js
+++ Project3/chess.js
@@ -4,19 +4,79 @@ function genDivs(v) {
     let a = document.getElementById("a");
     a.parentNode.removeChild(a);
 
-    var e = document.getElementById("game");
-    for (var i = 0; i < v; i++) {
-        var row = document.createElement("div");
+    let e = document.getElementById("game");
+    for (let i = 0; i < v; i++) {
+        let row = document.createElement("div");
         row.className = "row";
-        for (var x = 1; x <= v; x++) {
-            var cell = document.createElement("div");
+        for (let j = 1; j <= v; j++) {
+            let cell = document.createElement("div");
             cell.className = "gridsquare";
-            cell.innerText = (i * v) + x;
             row.appendChild(cell);
+            addChessPiece(i, j, cell);
+
         }
         e.appendChild(row);
     }
 }
+
+function addChessPiece(i, j, cell) {
+    if (i === 1) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-pawn fa-3x";
+        piece.style.color = "red";
+        cell.appendChild(piece);
+    } else if (i === 6) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-pawn fa-3x";
+        cell.appendChild(piece);
+    } else if (i === 0 && (j === 1 || j === 8)) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-rook fa-3x";
+        piece.style.color = "red";
+        cell.appendChild(piece);
+    } else if (i === 7 && (j === 1 || j === 8)) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-rook fa-3x";
+        cell.appendChild(piece);
+    } else if (i === 0 && (j === 2 || j === 7)) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-knight fa-3x";
+        piece.style.color = "red";
+        cell.appendChild(piece);
+    } else if (i === 7 && (j === 2 || j === 7)) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-knight fa-3x";
+        cell.appendChild(piece);
+    } else if (i === 0 && (j === 3 || j === 6)) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-bishop fa-3x";
+        piece.style.color = "red";
+        cell.appendChild(piece);
+    } else if (i === 7 && (j === 3 || j === 6)) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-bishop fa-3x";
+        cell.appendChild(piece);
+    } else if (i === 0 && j === 4) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-queen fa-3x";
+        piece.style.color = "red";
+        cell.appendChild(piece);
+    } else if (i === 7 && j === 4) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-queen fa-3x";
+        cell.appendChild(piece);
+    } else if (i === 0 && j === 5) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-king fa-3x";
+        piece.style.color = "red";
+        cell.appendChild(piece);
+    } else if (i === 7 && j === 5) {
+        let piece = document.createElement("i");
+        piece.className = "fas fa-chess-king fa-3x";
+        cell.appendChild(piece);
+    }
+}
+
 
 
 
